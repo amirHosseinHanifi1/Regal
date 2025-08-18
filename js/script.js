2f42259c9952de84054ac82b78e726ae568e86e7
amirHosseinHanifi1 <amirhanifi.1381@gmail.com>
2025-08-18 18:35:36 +0330

    last commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 var mobile_header_button = document.querySelector('.mobile-header-button-3')
-var desktop_header_nav_bar_2 = document.querySelector('.desktop-header-nav-bar-2')
+var search_button_desktop = document.querySelector('.search-button')
+var close_desktop_search_button = document.querySelector('.close-search-page-desktop')
+var desktop_sorting_navbar_button = document.querySelector('.desktop-sorting-navbar-button')
 
 
 mobile_header_button.addEventListener('focus', () => {
@@ -23,29 +25,51 @@ mobile_header_button.addEventListener('focus', () => {
     })
 })
 
-
-desktop_header_nav_bar_2.addEventListener('mouseenter', () => {
-    document.querySelector('.desktop-sorting-nav-bar').classList.remove('opacity-0')
-    document.querySelector('.desktop-sorting-nav-bar').classList.add('opacity-100')
+search_button_desktop.addEventListener('click', () => {
+    document.querySelector('.search-page-desktop').classList.remove('z-0')
+    document.querySelector('.search-page-desktop').classList.add('z-50')
+    document.querySelector('.search-page-desktop').classList.remove('opacity-0')
+    document.querySelector('.search-page-desktop').classList.add('opacity-100')
 
     document.querySelector('.dark-screen').classList.remove('hidden')
     document.querySelector('.dark-screen').classList.remove('opacity-0')
     document.querySelector('.dark-screen').classList.add('opacity-100')
 
-    desktop_header_nav_bar_2.classList.add('bg-[var(--color-gray-5)]')
+    document.querySelector('.search-page-desktop').addEventListener('mouseleave', () => {
+        document.querySelector('.search-page-desktop').classList.remove('z-50')
+        document.querySelector('.search-page-desktop').classList.add('z-0')
+        document.querySelector('.search-page-desktop').classList.remove('opacity-100')
+        document.querySelector('.search-page-desktop').classList.add('opacity-0')
 
-    desktop_header_nav_bar_2.querySelector ('img').setAttribute('src', '../icon/arrow-up.svg')
+        document.querySelector('.dark-screen').classList.remove('opacity-100')
+        document.querySelector('.dark-screen').classList.add('opacity-0')
+        document.querySelector('.dark-screen').classList.add('hidden')
+    })
+})
 
-    desktop_header_nav_bar_2.addEventListener('mouseleave', () => {
-        document.querySelector('.desktop-sorting-nav-bar').classList.remove('opacity-100')
-        document.querySelector('.desktop-sorting-nav-bar').classList.add('opacity-0')
+
+desktop_sorting_navbar_button.addEventListener('mouseenter', () => {
+    document.querySelector('.desktop-sorting-navbar').classList.remove('opacity-0')
+    document.querySelector('.desktop-sorting-navbar').classList.add('opacity-100')
+
+    document.querySelector('.dark-screen').classList.remove('hidden')
+    document.querySelector('.dark-screen').classList.remove('opacity-0')
+    document.querySelector('.dark-screen').classList.add('opacity-100')
+
+    desktop_sorting_navbar_button.classList.add('bg-[var(--color-gray-5)]')
+
+    desktop_sorting_navbar_button.querySelector ('img').setAttribute('src', '../icon/arrow-up.svg')
+
+    document.querySelector('.desktop-sorting-navbar').addEventListener('mouseleave', () => {
+        document.querySelector('.desktop-sorting-navbar').classList.remove('opacity-100')
+        document.querySelector('.desktop-sorting-navbar').classList.add('opacity-0')
 
         document.querySelector('.dark-screen').classList.remove('opacity-100')
         document.querySelector('.dark-screen').classList.add('opacity-0')
         document.querySelector('.dark-screen').classList.add('hidden')
 
-        desktop_header_nav_bar_2.classList.remove('bg-[var(--color-gray-5)]')
+        desktop_sorting_navbar_button.classList.remove('bg-[var(--color-gray-5)]')
 
-        desktop_header_nav_bar_2.querySelector ('img').setAttribute('src', '../icon/arrow-down.svg')
+        desktop_sorting_navbar_button.querySelector ('img').setAttribute('src', '../icon/arrow-down.svg')
     })
 })
