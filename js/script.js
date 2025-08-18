d22a45c0a2a1d36b9098abf4aa812130151f6d43
amirHosseinHanifi1 <amirhanifi.1381@gmail.com>
2025-08-18 09:57:19 +0330

    main-page-1 desktop done

--- js/script.js
+++ js/script.js
@@ -6,11 +6,19 @@ mobile_header_button.addEventListener('focus', () => {
     document.querySelector('.mobile-header-button-3 > img').setAttribute('src', '../icon/close-icon.svg')
     document.querySelector('.mobile-nav-bar').classList.remove('opacity-0')
     document.querySelector('.mobile-nav-bar').classList.add('opacity-100')
+
     document.querySelector('.dark-screen').classList.remove('hidden')
+    document.querySelector('.dark-screen').classList.remove('opacity-0')
+    document.querySelector('.dark-screen').classList.add('opacity-100')
+
     mobile_header_button.addEventListener('blur', () => {
         document.querySelector('.mobile-header-button-3 > img').setAttribute('src', '../icon/menu.svg')
+
         document.querySelector('.mobile-nav-bar').classList.remove('opacity-100')
         document.querySelector('.mobile-nav-bar').classList.add('opacity-0')
+
+        document.querySelector('.dark-screen').classList.remove('opacity-100')
+        document.querySelector('.dark-screen').classList.add('opacity-0')
         document.querySelector('.dark-screen').classList.add('hidden')
     })
 })
@@ -19,12 +27,25 @@ mobile_header_button.addEventListener('focus', () => {
 desktop_header_nav_bar_2.addEventListener('mouseenter', () => {
     document.querySelector('.desktop-sorting-nav-bar').classList.remove('opacity-0')
     document.querySelector('.desktop-sorting-nav-bar').classList.add('opacity-100')
+
     document.querySelector('.dark-screen').classList.remove('hidden')
+    document.querySelector('.dark-screen').classList.remove('opacity-0')
+    document.querySelector('.dark-screen').classList.add('opacity-100')
+
     desktop_header_nav_bar_2.classList.add('bg-[var(--color-gray-5)]')
-    document.querySelector('.desktop-sorting-nav-bar').addEventListener('mouseleave', () => {
+
+    desktop_header_nav_bar_2.querySelector ('img').setAttribute('src', '../icon/arrow-up.svg')
+
+    desktop_header_nav_bar_2.addEventListener('mouseleave', () => {
         document.querySelector('.desktop-sorting-nav-bar').classList.remove('opacity-100')
         document.querySelector('.desktop-sorting-nav-bar').classList.add('opacity-0')
-        desktop_header_nav_bar_2.classList.remove('bg-[var(--color-gray-5)]')
+
+        document.querySelector('.dark-screen').classList.remove('opacity-100')
+        document.querySelector('.dark-screen').classList.add('opacity-0')
         document.querySelector('.dark-screen').classList.add('hidden')
+
+        desktop_header_nav_bar_2.classList.remove('bg-[var(--color-gray-5)]')
+
+        desktop_header_nav_bar_2.querySelector ('img').setAttribute('src', '../icon/arrow-down.svg')
     })
 })
--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,12 @@
 export default {
   plugins: ["@tailwindcss/postcss"],
 }
+module.exports = {
+  content: ["./public/index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('tailwind-scrollbar'),
+  ],
+}
